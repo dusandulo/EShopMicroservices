93e7203d3a87fe89fa189f80c36f6d97ec68c84e
dusandulo <borovicanindusan@gmail.com>
2025-03-09 17:33:10 +0100

    Refactor and add exception handling middleware

--- src/Services/Catalog/CatalogAPI/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/UpdateProduct/UpdateProductHandler.cs
@@ -2,17 +2,17 @@
 namespace CatalogAPI.Products.UpdateProduct
 {
     public record UpdateProductCommand(
-        Guid Id, 
-        string Name, 
-        List<string> Category, 
-        string Description, 
-        string ImageFile, 
+        Guid Id,
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
         decimal Price)
         : ICommand<UpdateProductResult>;
 
     public record UpdateProductResult(bool isSuccess);
-    public class UpdateProductCommandValidator 
-        :AbstractValidator<UpdateProductCommand>
+    public class UpdateProductCommandValidator
+        : AbstractValidator<UpdateProductCommand>
     {
         public UpdateProductCommandValidator()
         {
@@ -22,31 +22,32 @@ namespace CatalogAPI.Products.UpdateProduct
                 .NotEmpty().WithMessage("Product Name is required")
                 .Length(2, 150).WithMessage("Product Name must be between 2 and 150 characters");
 
-            RuleFor(command => command.Price)    
+            RuleFor(command => command.Price)
                 .GreaterThan(0).WithMessage("Product Price must be greater than 0");
         }
         internal class UpdateProductCommandHandler
         (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
         : ICommandHandler<UpdateProductCommand, UpdateProductResult>
-    {
-        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
         {
-            logger.LogInformation("UpdateProductHandler.Handler called with {@Command}", command);
-            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
-            if (product == null)
+            public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
             {
-                throw new ProductNotFoundException();
+                logger.LogInformation("UpdateProductHandler.Handler called with {@Command}", command);
+                var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+                if (product == null)
+                {
+                    throw new ProductNotFoundException();
+                }
+                product.Name = command.Name;
+                product.Category = command.Category;
+                product.Description = command.Description;
+                product.ImageFile = command.ImageFile;
+                product.Price = command.Price;
+
+                session.Update(product);
+                await session.SaveChangesAsync(cancellationToken);
+
+                return new UpdateProductResult(true);
             }
-            product.Name = command.Name;
-            product.Category = command.Category;
-            product.Description = command.Description;
-            product.ImageFile = command.ImageFile;
-            product.Price = command.Price;
-
-            session.Update(product);
-            await session.SaveChangesAsync(cancellationToken);
-
-            return new UpdateProductResult(true);
         }
     }
 }
