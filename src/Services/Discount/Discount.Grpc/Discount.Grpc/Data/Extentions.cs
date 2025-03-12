3fdf47766d1f232131eafb65b05530cb17cb7265
dusandulo <borovicanindusan@gmail.com>
2025-03-12 15:29:44 +0100

    Add SQLite support and replace Greeter with Discount service
    Updated project file to include EF Core SQLite packages and configure DiscountContext with SQLite. Removed greet.proto and GreeterService. Added discount.proto and DiscountService. Updated appsettings.json with SQLite connection string. Added initial migration and seed data for Coupons.

---
+++ src/Services/Discount/Discount.Grpc/Discount.Grpc/Data/Extentions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public static class Extentions
+    {
+        public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+        {
+            using var scopre = app.ApplicationServices.CreateScope();
+            using var dbContext = scopre.ServiceProvider.GetRequiredService<DiscountContext>();
+            dbContext.Database.MigrateAsync();
+
+            return app;
+        }
+    }
+}
