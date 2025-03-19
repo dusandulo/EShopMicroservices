bfe9e74fc15248193107bdadc56a3f16469eb448
dusandulo <borovicanindusan@gmail.com>
2025-03-19 12:23:16 +0100

    Add Ordering functionality with API, services, and Docker support
    Introduced new projects for Ordering, updated configurations, and added Dockerfile for containerization.

---
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services)
+        {
+            //services.AddMediatR(cfg =>{
+            //      cfg.RegisterServicesFromAssembly(Assembly.GeExecutingAssembly());
+            //});
+
+            return services;
+        }
+    }
+}
