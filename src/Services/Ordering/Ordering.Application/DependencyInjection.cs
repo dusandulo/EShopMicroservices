0004bb408fff176a3476b96f5ff31cd7ae538597
dusandulo <borovicanindusan@gmail.com>
2025-03-25 11:41:56 +0100

    Enhance dependency injection and add domain event handling
    Added MediatR registration, new interceptors, and domain event dispatching logic.

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Ordering.Application
 {
@@ -6,9 +7,10 @@ namespace Ordering.Application
     {
         public static IServiceCollection AddAplicationServices(this IServiceCollection services)
         {
-            //services.AddMediatR(cfg =>{
-            //      cfg.RegisterServicesFromAssembly(Assembly.GeExecutingAssembly());
-            //});
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
 
             return services;
         }
